3be0b884798eb78fc26ec796f247351128561230
InsertyEXE <insertyend@gmail.com>
2022-05-25 01:13:22 -0300

    feat: layout em progresso, replace fragment da loja e do perfil funcionando

--- app/src/main/java/com/example/steamremaked/main/view/MainActivity.kt
+++ app/src/main/java/com/example/steamremaked/main/view/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.steamremaked.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.steamremaked.R
 import com.example.steamremaked.databinding.ActivityMainBinding
+import com.example.steamremaked.main.view.perfil.view.PerfilFragment
+
+private lateinit var binding: ActivityMainBinding
+private lateinit var perfilFragment: PerfilFragment
+private lateinit var lojaFragment: LojaFragment
+private var currentFragment: Fragment? = null
+
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        lojaFragment = LojaFragment()
+        perfilFragment = PerfilFragment()
+        replaceFragment(R.id.fragment, perfilFragment)
 
         with(binding) {
 
@@ -23,13 +33,45 @@ class MainActivity : AppCompatActivity() {
 
             mainNav.setNavigationItemSelectedListener { menuItem ->
 
-                menuItem.isChecked
 
+                when(menuItem.itemId){
+                    R.id.menu_main_perfil -> {
+                        if (currentFragment == perfilFragment) false
+                        currentFragment = perfilFragment
+                    }
+                    R.id.menu_main_loja -> {
+                        if (currentFragment == lojaFragment) false
+                        currentFragment = lojaFragment
+                    }
+                }
+
+                currentFragment?.let {
+                    replaceFragment(R.id.fragment, it)
+                }
+
+                menuItem.isChecked
                 mainDrawer.close()
                 true
             }
         }
 
+
+    }
+
+    private fun replaceFragment(id: Int, fragment: Fragment) {
+
+        if (supportFragmentManager.findFragmentById(id) == null) {
+            supportFragmentManager.beginTransaction().apply {
+                add(id, fragment)
+                commit()
+            }
+        } else {
+            supportFragmentManager.beginTransaction().apply {
+                replace(id, fragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
 
     }
 }
